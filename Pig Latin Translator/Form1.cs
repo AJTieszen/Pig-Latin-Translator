77b9a375461226256aed4b78effc181d9b6ba0d3
Andrew Tieszen <pilottzn@gmail.com>
2023-09-21 20:25:10 -0600

    Created skeleton translate function

--- Pig Latin Translator/Form1.cs
+++ Pig Latin Translator/Form1.cs
@@ -20,6 +20,7 @@ namespace Pig_Latin_Translator
         private void btnClear_Click_1(object sender, EventArgs e)
         {
             englishText.Clear();
+            pigLatinText.Clear();
         }
 
         private void btnExit_Click_1(object sender, EventArgs e)
@@ -33,12 +34,25 @@ namespace Pig_Latin_Translator
             if (!string.IsNullOrEmpty(english))
             {
                 string[] words = english.Split(' ');
+                string pigLatin = "";
+
+                foreach (string word in words)
+                {
+                    pigLatin += translateWord(word) + " ";
+                }
+
+                pigLatinText.Text = pigLatin;
             }
             else
             {
                 MessageBox.Show("You must enter some text to translate.", "Enter text");
                 englishText.Focus();
             }
+        }
+
+        private string translateWord(string word)
+        {
+            return word;
         }
     }
 }
